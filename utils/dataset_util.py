1c6ba1f1aea3e0a882c8bb56008696ecb2f2a215
matin <mmgn46@yahoo.com>
2024-01-06 16:43:01 +0330

    dataset_util.py: chunk pad tokens changed into two functions. map ids to charecters added for better visulaization of tokenization

--- utils/dataset_util.py
+++ utils/dataset_util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 
 def summery_chunked(input_ids_list, attention_mask_list, labels_list, show_data=False):
@@ -26,7 +27,29 @@ def summery_chunked(input_ids_list, attention_mask_list, labels_list, show_data=
               f'Unique types in attention_mask_list: {count_unique_types(attention_mask_list)}\n\n')
 
 
-def chunk_pad_tokens(tokens, labels, chunk_size=512, tok_pad=0, label_pad=-100, attention_pad=0, summary=False):
+def chunk_sentence(tokens: list, labels: list, chunk_size: int = 512) -> tuple:
+    """
+    Chunks a list of tokens and labels into smaller lists of size `chunk_size`.
+
+    Args:
+        tokens (list): A list of tokens.
+        labels (list): A list of labels corresponding to the tokens.
+        chunk_size (int, optional): The size of each chunk. Defaults to 512.
+
+    Returns:
+        tuple: A tuple containing two lists - one containing the chunked tokens and the other containing the corresponding labels.
+    """
+    if len(tokens) != len(labels):
+        raise Exception("list sizes do not match")
+    chunked_tokens = []
+    chunked_labels = []
+    for i in range(0, len(tokens), chunk_size):
+        chunked_tokens.append(tokens[i:i + chunk_size])
+        chunked_labels.append(labels[i:i + chunk_size])
+    return chunked_tokens, chunked_labels
+
+def chunk_pad_tokens(tokens, labels, chunk_size=512, padd=True, tok_pad=0, label_pad=-100, attention_pad=0,
+                               summary=False):
     input_ids_list = []
     attention_mask_list = []
     labels_list = []
@@ -37,10 +60,10 @@ def chunk_pad_tokens(tokens, labels, chunk_size=512, tok_pad=0, label_pad=-100, 
         chunk_attention_mask = [1] * len(chunked_tokens)
         chunk_label_ids = labels[i:i + chunk_size]
 
-        while len(chunked_tokens) < chunk_size:
-            chunked_tokens.append(0)
-            chunk_attention_mask.append(0)
-            chunk_label_ids.append(-100)
+        while padd and len(chunked_tokens) < chunk_size:
+            chunked_tokens.append(tok_pad)
+            chunk_attention_mask.append(attention_pad)
+            chunk_label_ids.append(label_pad)
         input_ids_list.append(chunked_tokens)
         attention_mask_list.append(chunk_attention_mask)
         labels_list.append(chunk_label_ids)
@@ -55,64 +78,25 @@ def remove_large_elements(lst, threshold):
     return lst
 
 
-class Dataset_Builder:
-    def __init__(self, tokenizer):
-        self.tokenizer = tokenizer
-        pass
+def map_ids_to_chars(ids: list[int], chars: list[str]) -> dict[int, list[str]]:
+    """
+    Maps IDs to their corresponding characters.
 
-    def chunck_tokens(self, tokens, labels, chunk_size=512, summery=False):
-        input_ids_list = []
-        attention_mask_list = []
-        labels_list = []
+    Args:
+        ids: A list of integers representing the IDs to map.
+        chars: A list of strings representing the corresponding characters.
 
-        # Create chunks
-        for i in range(0, len(tokens), 512 - 2):  # We subtract 2 to account for special tokens
-            chunk_tokens = tokens[i:i + 512 - 2]
-            chunk_label_ids = labels[i:i + 512 - 2]
+    Returns:
+        A dictionary where each ID is mapped to a list of all the corresponding characters.
+    """
+    result = {}
+    for i in range(len(ids)):
+        if ids[i] not in result:
+            result[ids[i]] = []
+        for j in range(len(chars[i])):
+            result[ids[i]].append(chars[i][j])
+    return result
 
-            # Add special tokens
-            chunk_tokens = [self.tokenizer.encode('[CLS]')[1]] + chunk_tokens + [self.tokenizer.encode('[SEP]')[1]]
-            # chunk_tokens = ['[CLS]'] + chunk_tokens + ['[SEP]']
-            chunk_label_ids = [-100] + chunk_label_ids + [-100]
 
-            # Convert tokens to input IDs and create attention mask
-
-            chunk_attention_mask = [1] * len(chunk_tokens)
-
-            # Pad sequenceschunk_input_ids = tokenizer.convert_tokens_to_ids(chunk_tokens)
-            while len(chunk_tokens) < chunk_size:
-                chunk_tokens.append(0)
-                chunk_attention_mask.append(0)
-                chunk_label_ids.append(-100)
-
-            input_ids_list.append(chunk_tokens)
-            attention_mask_list.append(chunk_attention_mask)
-            labels_list.append(chunk_label_ids)
-        if summery:
-            self.summery_chuncked(input_ids_list, attention_mask_list, labels_list)
-        return input_ids_list, attention_mask_list, labels_list
-
-    @staticmethod
-    def summery_chuncked(input_ids_list, attention_mask_list, labels_list, show_data=False):
-        def count_unique_types(two_d_list):
-            unique_types = set()
-            for sublist in two_d_list:
-                for item in sublist:
-                    unique_types.add(type(item))
-            return unique_types
-
-        mask_shape = np.array(attention_mask_list).shape
-        ids_shape = np.array(input_ids_list).shape
-        lbl_shape = np.array(labels_list).shape
-        print("Shapes\nIDs:\t\t\t", ids_shape)
-        print("Labels:\t\t\t", lbl_shape)
-        print("Attention Mask:\t", mask_shape)
-
-        # Example usage:
-        if show_data:
-            print('input_ids\n', np.array(input_ids_list))
-            print('\n\n\n\nlabels_list\n', np.array(labels_list))
-            print('\n\n\n\nattention_mask_list\n', np.array(attention_mask_list))
-            print(count_unique_types(input_ids_list))  # Output: 4
-            print(lbl_shape, count_unique_types(labels_list))  # Output: 4
-            print(mask_shape, count_unique_types(attention_mask_list))  # Output: 4
+def flatten_2d_list(lst):
+    return list(itertools.chain.from_iterable(lst))
